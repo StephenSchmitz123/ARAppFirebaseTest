60f66c8748ca83db6d43b0242f959e28bf0f10ab
StephenSchmitz <87332097+StephenSchmitz123@users.noreply.github.com>
2021-08-23 15:31:28 -0400

    Commit new functions createPastProjects, createFunFacts and so on...
    Commit new functions createPastProjects, createFunFacts and so on...

--- functions/index.js
+++ functions/index.js
@@ -25,6 +25,51 @@ exports.createMethotar = functions.https.onRequest( async (req, res) => {
     return res.send('completed');
 });
 
+exports.createPastProjects = functions.https.onRequest( async (req, res) => {
+    const data = req.query; 
+    const clientID = data.client.id
+    const dateStarted = data.dateStarted;
+    const dateEnded = data.dateEnded;
+    const description = data.description;
+    
+    const teamRef = db.collection("team");
+    const team = data.team
+
+    const docRef = await db.collection('pastprojects').doc(userID);
+    await docRef.set({
+        'clientID': clientID,
+        'dateStarted': dateStarted,
+        'dateEnded': dateEnded,
+        'description': description,
+    });
+
+    for(member in team){
+        docRef.teamRef.doc(member.id).set({
+            'fName': member.firstName,
+            'lName': member.lastName,
+            'jobDescription': member.jobDescripiton,
+            'bio': member.bio
+        })
+    }
+
+    return res.send('completed');
+});
+
+exports.createFunFacts = functions.https.onRequest( async (req, res) => {
+    const data = req.query; 
+    const title = data.title;
+    const description = data.description;
+
+    const docRef = await db.collection('funfacts').doc(userID);
+    
+    await docRef.set({
+        'title': title,
+        'description': description,
+    });
+
+    return res.send('completed');
+});
+
 exports.getMethotar = functions.https.onRequest( async (req, res) => {
     const userID = req.query.userID;
     const user = await db.collection('methotar').doc(userID).get(); 
@@ -33,13 +78,31 @@ exports.getMethotar = functions.https.onRequest( async (req, res) => {
 });
 
 exports.getFunFacts = functions.https.onRequest( async (req, res) => {
-    return res.json('hello fun facts');
+    const userID = req.query.userID;
+
+    const user = await db.collection('funfacts').doc(userID).get(); 
+    console.log(user.data());
+    return res.send({
+        title: user.data().title,
+        description: user.data().description,
+    });
 });
 
 exports.getPastProjects = functions.https.onRequest( async (req, res) => {
-    return res.json('hello past project');
+    const userID = req.query.userID;
+    
+    const user = await db.collection('pastproject').doc(userID).get(); 
+    console.log(user.data());
+    
+    return res.send({
+        clientID: user.data().clientID,
+        dateStartedtID: user.data().dateStarted,
+        dateEnded: user.data().dateEnded,
+        description: user.data().description,
+        team: user.data().team,
+    });
 });
 
 exports.getLeaderboard = functions.https.onRequest( async (req, res) => {
-    return res.json('hello leaderboard');
+    return res.send('completed');
 });
